f6a6be8cb6de81ee0bd694694de388317fddbb99
Brandon Vergara <brandon@makingdevs.com>
2022-02-11 18:17:11 -0600

    [main] add package to read qr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QrReader from 'react-qr-scanner'
 
 function App() {
+  const [result, setResult] = useState('No result');
+  const [delay] = useState(100);
+
+  function handleScan(data) {
+    if (data) {
+      setResult(data)
+    }
+  }
+  function handleError(err) {
+    console.error(err)
+  }
+
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +33,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        Learn React
+        <QrReader
+          delay={delay}
+          style={previewStyle}
+          onError={handleError}
+          onScan={handleScan}
+        />
+        <p>{result}</p>
       </header>
     </div>
   );
