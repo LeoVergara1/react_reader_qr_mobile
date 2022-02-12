14dbfea852ff57f12a930ce88d8e071495b3431f
Brandon Vergara <brandon@makingdevs.com>
2022-02-12 16:47:31 -0600

    [main] add select cam

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import QrReader from 'react-qr-scanner'
@@ -6,6 +6,27 @@ import QrReader from 'react-qr-scanner'
 function App() {
   const [result, setResult] = useState('No result');
   const [delay] = useState(100);
+  const [devices, setDevices] = useState([]);
+  const [cameraId, setCameraId] = useState(0);
+
+  useEffect(() => {
+    navigator?.mediaDevices.enumerateDevices()
+    .then((devices_result) => {
+      const videoSelect = []
+      devices_result.forEach((device) => {
+        if (device.kind === 'videoinput') {
+          videoSelect.push(device)
+        }
+      })
+      return videoSelect
+    })
+    .then((devices_result) => {
+      setDevices(devices_result);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, []);
 
   function handleScan(data) {
     if (data) {
@@ -15,6 +36,8 @@ function App() {
   function handleError(err) {
     console.error(err)
   }
+
+  const selectCamera = () => cameraId;
 
   const previewStyle = {
     height: 240,
@@ -40,8 +63,19 @@ function App() {
           style={previewStyle}
           onError={handleError}
           onScan={handleScan}
+          chooseDeviceId={selectCamera}
         />
         <p>{result}</p>
+        <select
+          onChange={e => {
+            const value = e.target.value
+            setCameraId(value)
+          }}
+        >
+          {devices.map((deviceInfo, index) => (
+            <React.Fragment key={deviceInfo.deviceId}><option value={deviceInfo.deviceId}>{deviceInfo.label || `camera ${index}`}</option></React.Fragment>
+          ))}
+        </select>
       </header>
     </div>
   );
